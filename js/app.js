09cb6c6854a55465e7c2368605051ff77037cec2
gotgotgot <gautier.mejane@epfedu.fr>
2016-03-16 00:55:52 +0100

    Added files via upload

--- js/app.js
+++ js/app.js
@@ -1,13 +1,36 @@
 (function(){
  
   var app = angular.module('myApp', []);
+
    app.controller('StoreController', ['$http', function($http){
+
   var store = this;
   store.products = [];
-  $http.get('http://localhost/projet_webdesign_AD_GM-master/json/vehicules-commercialises.json').success(function(data){
+  $http.get('http://localhost/projet_webdesign_AD_GM-gotgotgot-patch-1/json/vehicules-commercialises.json').success(function(data){
     store.products = data;
   });
+
+this.isSelected=function(modeleGiven){
+modeleGiven=this.product.fields.marque_modele;
+return;
+};
+
 }]);
+
+ app.controller('TabController',function(){
+    this.tab=1;
+    this.isSet=function(givenTab){
+      return this.tab===givenTab;
+    };
+    this.setTab=function(selectedTab){
+                          this.tab=selectedTab;
+                          };
+    
+  });
+
+ 
+  
+//Controlleur vérifiant le caractère unique
 
 
 
